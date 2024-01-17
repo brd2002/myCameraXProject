48ff7bb17b14ad5c9b86841542d8f79d2e09a9dd
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-17 22:02:39 +0530

    this is my commit in camarax project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My_first_camerax_project"
+include(":app")
+ 
